21ecb0a19c93ead334e4f3530edfa0f8fca8dcc0
Wilmer-Vnr <wilmer.vargas@epn.edu.ec>
2025-04-22 16:57:29 -0500

    segundo taller

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+// para utilizar el servicio en tab2
+
+import { PhotoService } from "../services/photo.service";
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: false,
+})
+export class Tab2Page {
+
+  constructor(public photoService: PhotoService) { }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery()
+  }
+}
+
+
